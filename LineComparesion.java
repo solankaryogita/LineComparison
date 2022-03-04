16a248b44dab0cf30932fcbc1b8bce7916994fc6
PoojaSolankar <poojasolankar09gmail.com>
2022-03-05 00:42:53 +0530

    Length of Line

--- LineComparesion.java
+++ LineComparesion.java
@@ -1,13 +1,21 @@
 package com.Bridgelabz;
 
 import java.util.*;
+import java.lang.Math.*;
+
 
 public class LineComparesion {
 
 	public static void main(String[] args) {
 
 		    System.out.println("Welcome to Line comparison ");
-		    
+		    Integer x1=2 , x2=4 , y1=4 , y2=6 ;
+
+		    Double AB;
+
+		    AB = Math.sqrt((x2-x1)^2+(y2-y1)^2);
+		    System.out.println("Legnth of l1="+AB);
+
 	}
 }
 
