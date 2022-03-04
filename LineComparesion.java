bbda093780050441423984278885a0ac39396bf4
PoojaSolankar <poojasolankar09gmail.com>
2022-03-05 00:20:21 +0530

    Welcome to line comparison

---
+++ LineComparesion.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz;
+
+import java.util.*;
+import java.lang.Math.*;
+
+
+public class LineComparesion {
+
+	public static void main(String[] args) {
+
+		    System.out.println("Welcome to Line comparison computation");
+		    
+	}
+}
+
